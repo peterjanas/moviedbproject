7a8ccb8315cf7a5a7975c34a3dd229143a1e95b6
peterjanas <pejanas@gmail.com>
2024-09-17 14:13:00 +0200

    yug

--- src/main/java/app/entity/MoviePersonnel.java
+++ src/main/java/app/entity/MoviePersonnel.java
@@ -13,6 +13,7 @@ public class MoviePersonnel
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
+    private Long id;
 
     @ManyToOne
     private Movie movie;
