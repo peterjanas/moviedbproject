e0f75bc0bedcbc5d5984b516f7a329933c7eb263
peterjanas <pejanas@gmail.com>
2024-09-17 12:45:39 +0200

    enums

--- src/main/java/app/entity/Movie.java
+++ src/main/java/app/entity/Movie.java
@@ -1,5 +1,10 @@
 package app.entity;
 
+
+import lombok.AccessLevel;
+import lombok.Setter;
+
+@Setter
 public class Movie
 {
 }
--- src/main/java/app/enums/Genre.java
+++ src/main/java/app/enums/Genre.java
@@ -1,6 +1,49 @@
 package app.enums;
 
-public enum Genre
-{
-    ACTION, COMEDY, DRAMA, FANTASY, HORROR, MYSTERY, ROMANCE, THRILLER
+public enum Genre {
+    ACTION(28, "Action"),
+    ADVENTURE(12, "Adventure"),
+    ANIMATION(16, "Animation"),
+    COMEDY(35, "Comedy"),
+    CRIME(80, "Crime"),
+    DOCUMENTARY(99, "Documentary"),
+    DRAMA(18, "Drama"),
+    FAMILY(10751, "Family"),
+    FANTASY(14, "Fantasy"),
+    HISTORY(36, "History"),
+    HORROR(27, "Horror"),
+    MUSIC(10402, "Music"),
+    MYSTERY(9648, "Mystery"),
+    ROMANCE(10749, "Romance"),
+    SCIENCE_FICTION(878, "Science Fiction"),
+    TV_MOVIE(10770, "TV Movie"),
+    THRILLER(53, "Thriller"),
+    WAR(10752, "War"),
+    WESTERN(37, "Western");
+
+    private final int id;
+    private final String name;
+
+    Genre(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Genre getById(int id) {
+        for (Genre genre : values()) {
+            if (genre.getId() == id) {
+                return genre;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Genre ID: " + id);
+    }
 }
+
