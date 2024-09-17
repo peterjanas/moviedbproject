be72d73ddf4323fb0f318f921f43b1786e0ec951
HenrikThunbo10 <henningthunbo@hotmail.com>
2024-09-17 13:54:31 +0200

    personneldto

--- src/main/java/app/dto/PersonnelDTO.java
+++ src/main/java/app/dto/PersonnelDTO.java
@@ -1,5 +1,20 @@
 package app.dto;
 
+import app.entity.MoviePersonnel;
+import app.enums.Role;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
 public class PersonnelDTO
 {
+    private Long id;
+    private String name;
+    private int roleID;
+    private Role role;
+    private Set<MoviePersonnel> moviePersonnel = new HashSet<>();
 }
