4d3bb227f4d68e61ad3551ba74877f872e5ef56a
peterjanas <pejanas@gmail.com>
2024-09-17 14:11:53 +0200

    MoviePersonnel ting

--- src/main/java/app/entity/MoviePersonnel.java
+++ src/main/java/app/entity/MoviePersonnel.java
@@ -13,18 +13,14 @@ public class MoviePersonnel
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Long id;
-    private Long movieID;
-    private Long personnelID;
+
     @ManyToOne
     private Movie movie;
     @ManyToOne
     private Personnel personnel;
 
-    public MoviePersonnel(Long movieID, Long personnelID, Movie movie, Personnel personnel)
+    public MoviePersonnel(Movie movie, Personnel personnel)
     {
-        this.movieID = movieID;
-        this.personnelID = personnelID;
         this.movie = movie;
         this.personnel = personnel;
     }
