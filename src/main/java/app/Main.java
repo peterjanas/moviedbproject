58032c45d6e619020a0b14899257f7c1693790f8
Umair-230104 <cph-ut38@cphbusiness.dk>
2024-09-18 15:04:16 +0200

    gemmer movies til db

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,5 @@
 package app;
 
-import app.config.HibernateConfig;
-import jakarta.persistence.EntityManagerFactory;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -12,7 +9,7 @@ public class Main
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException
     {
 
-      EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("moviedb");
+//      EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("moviedb");
 
 
 
@@ -37,20 +34,21 @@ public class Main
 
 
 
-
-      // Masih test
-      MovieDAO movieDAO = new MovieDAO(emf);
-      System.out.println(movieDAO.getTop10LowestRatedMovies());
-
-
-
+//
+//      // Masih test
+//      MovieDAO movieDAO = new MovieDAO(emf);
+//      System.out.println(movieDAO.getTop10LowestRatedMovies());
+//
 
 
 
 
 
-      MovieService movieService = new MovieService();
-//        movieService.getMovies(5);
+
+
+//      MovieService movieService = new MovieService();
+////        movieService.getMovies(5);
+
 
 
 
