df855bfc1370a24e338a65723f927ca8edc46ed5
Umair-230104 <cph-ut38@cphbusiness.dk>
2024-09-19 11:33:04 +0200

    movie og personl

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,10 @@
 package app;
 
+import app.config.HibernateConfig;
+import app.dao.MovieDAO;
+import app.dto.MovieDTO;
 import app.service.MovieService;
-import app.service.PersonnelService;
+import jakarta.persistence.EntityManagerFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -11,42 +14,21 @@ public class Main
 {
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException
     {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("moviedb");
 
-//      EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("moviedb");
+//Masih test
+//        MovieDAO movieDAO = new MovieDAO(emf);
+//        System.out.println(movieDAO.getTop10LowestRatedMovies());
 
+// Personnel Test
+//        PersonnelService personnelService = new PersonnelService();
+//        System.out.println(personnelService.getPersonnel(646097));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//      // Masih test
-//      MovieDAO movieDAO = new MovieDAO(emf);
-//      System.out.println(movieDAO.getTop10LowestRatedMovies());
-//
-
-
-        PersonnelService personnelService = new PersonnelService();
-        System.out.println(personnelService.getPersonnel(646097));
-
-
+//Movie Test
+        MovieService movieService = new MovieService();
+        movieService.getAllMoviesAndTheirPersonnel();
+        MovieDAO movieDAO = new MovieDAO(emf);
+        List<MovieDTO> movieList = movieService.getAllMoviesForDatabase();
+        movieDAO.saveMoviesToDb(movieList);
     }
 }
